368bfe1df9154ffc39e9e3233f3d59545c2f2075
ruthjuma100@gmail.com <ruthjuma100@gmail.com>
2026-03-27 12:19:24 +0300

    used global, function and block scope to integrate functionality to the menu

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+const burgers = ["Hamburger", "Cheeseburger"]
+
+function addBurger(){
+    const newBurger = "Flatburger"
+    burgers.push(newBurger);
+}
+
+if(true){const anotherNewBurger="Maple Bacon Burger";burgers.push(anotherNewBurger);}
+
+let featuredDrink = "Strawberry Milkshake"
+
+function changeFeaturedDrink(){
+    featuredDrink = "The JavaShake"
+}
+
+
+
+   
